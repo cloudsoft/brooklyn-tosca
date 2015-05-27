d1f6cca9c078b7510accc5f93dc8dcda7e859493
Luc Boutier <luc.boutier@me.com>
2015-05-27 11:42:20 +0200

    cleanup.

--- src/test/resources/components/artifact-test/scripts/configureProperties.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$properties_file" ]; then
-    echo "properties_file is not set, test failed"
-    exit 1
-fi
-echo "war_file is set to $properties_file"
--- src/test/resources/components/artifact-test/scripts/create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$war_file" ]; then
-    echo "war_file is not set, test failed"
-    exit 1
-fi
-echo "war_file is set to $war_file"

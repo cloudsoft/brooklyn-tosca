e24c8fc4db3fdd3f85acebd7548ed46b5c19f658
Luc Boutier <luc.boutier@me.com>
2015-05-27 11:40:00 +0200

    Update tests and types to be used with brooklyn.

---
+++ src/test/java/alien4cloud/brooklyn/PluginTest.java
@@ -0,0 +1,19 @@
+package alien4cloud.brooklyn;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-context.xml")
+@Slf4j
+public class PluginTest {
+    @Test
+    public void testPlugin() {
+        log.info("start test");
+    }
+}

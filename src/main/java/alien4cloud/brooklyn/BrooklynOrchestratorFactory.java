a4826aae5b1e57673fec6841c9bf4a49bf765b69
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-10-07 16:30:46 +0200

    Fix unable to delete configuration value

--- src/main/java/alien4cloud/brooklyn/BrooklynOrchestratorFactory.java
+++ src/main/java/alien4cloud/brooklyn/BrooklynOrchestratorFactory.java
@@ -6,31 +6,31 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import alien4cloud.model.components.PropertyDefinition;
 import alien4cloud.model.orchestrators.ArtifactSupport;
 import alien4cloud.model.orchestrators.locations.LocationSupport;
 import alien4cloud.orchestrators.plugin.IOrchestratorPluginFactory;
-import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component("brooklyn-orchestrator-factory")
-public class BrooklynOrchestratorFactory implements IOrchestratorPluginFactory<BrooklynOrchestrator, Configuration>{
-	
+public class BrooklynOrchestratorFactory implements IOrchestratorPluginFactory<BrooklynOrchestrator, Configuration> {
+
     @Autowired
     private BeanFactory beanFactory;
 
-	@Override
-	public BrooklynOrchestrator newInstance() {
-		BrooklynOrchestrator instance = beanFactory.getBean(BrooklynOrchestrator.class);
+    @Override
+    public BrooklynOrchestrator newInstance() {
+        BrooklynOrchestrator instance = beanFactory.getBean(BrooklynOrchestrator.class);
         log.info("Init brooklyn provider and beanFactory is {}", beanFactory);
         return instance;
-	}
+    }
 
-	@Override
-	public void destroy(BrooklynOrchestrator instance) {
+    @Override
+    public void destroy(BrooklynOrchestrator instance) {
         log.info("DESTROYING (noop)", instance);
-	}
+    }
 
     @Override
     public Class<Configuration> getConfigurationType() {
@@ -39,22 +39,22 @@ public class BrooklynOrchestratorFactory implements IOrchestratorPluginFactory<B
 
     @Override
     public Configuration getDefaultConfiguration() {
-        return new Configuration();
+        return new Configuration("http://localhost:8081/", "brooklyn", "brooklyn", "localhost");
     }
 
-	@Override
-	public LocationSupport getLocationSupport() {
-		return new LocationSupport(true, new String[]{});
-	}
+    @Override
+    public LocationSupport getLocationSupport() {
+        return new LocationSupport(true, new String[] {});
+    }
 
-	@Override
-	public ArtifactSupport getArtifactSupport() {
-		return new ArtifactSupport();
-	}
+    @Override
+    public ArtifactSupport getArtifactSupport() {
+        return new ArtifactSupport();
+    }
 
-	@Override
-	public Map<String, PropertyDefinition> getDeploymentPropertyDefinitions() {
-		return null;
-	}
+    @Override
+    public Map<String, PropertyDefinition> getDeploymentPropertyDefinitions() {
+        return null;
+    }
 
 }

a374c0e45a4770607574e46cfe91116473f60323
Kiuby88 <kiuby88.programming@gmail.com>
2015-10-30 11:41:48 +0100

    Fixing some Test issues. CampPlatform was added.

--- brooklyn-tosca-transformer/src/test/java/org/apache/brooklyn/tosca/a4c/Alien4CloudToscaTest.java
+++ brooklyn-tosca-transformer/src/test/java/org/apache/brooklyn/tosca/a4c/Alien4CloudToscaTest.java
@@ -1,8 +1,23 @@
 package org.apache.brooklyn.tosca.a4c;
 
+import org.apache.brooklyn.camp.CampPlatform;
+import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatformLauncherNoServer;
 import org.apache.brooklyn.core.test.BrooklynAppUnitTestSupport;
+import org.testng.annotations.BeforeMethod;
 
 public class Alien4CloudToscaTest extends BrooklynAppUnitTestSupport {
+
+    protected CampPlatform campPlatform;
+
+    @BeforeMethod
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        campPlatform = new BrooklynCampPlatformLauncherNoServer()
+                .useManagementContext(mgmt)
+                .launch()
+                .getCampPlatform();
+    }
 
     public String getClasspathUrlForResource(String resourceName){
         return "classpath://"+ resourceName;

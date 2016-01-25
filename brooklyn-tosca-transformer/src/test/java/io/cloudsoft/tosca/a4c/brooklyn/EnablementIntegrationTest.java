975ac96577307a09f66321e73e43b52f4b32ac14
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2016-01-25 11:20:48 +0000

    PlanToSpecTransformer throws PlanNotRecognisedException rather than IllegalState

--- brooklyn-tosca-transformer/src/test/java/io/cloudsoft/tosca/a4c/brooklyn/EnablementIntegrationTest.java
+++ brooklyn-tosca-transformer/src/test/java/io/cloudsoft/tosca/a4c/brooklyn/EnablementIntegrationTest.java
@@ -8,6 +8,7 @@ import org.apache.brooklyn.api.entity.EntitySpec;
 import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatformLauncherNoServer;
 import org.apache.brooklyn.camp.brooklyn.spi.creation.CampTypePlanTransformer;
 import org.apache.brooklyn.core.BrooklynFeatureEnablement;
+import org.apache.brooklyn.core.plan.PlanNotRecognizedException;
 import org.apache.brooklyn.core.test.BrooklynAppUnitTestSupport;
 import org.apache.brooklyn.core.typereg.RegisteredTypeLoadingContexts;
 import org.apache.brooklyn.util.core.ResourceUtils;
@@ -21,7 +22,7 @@ import io.cloudsoft.tosca.a4c.platform.Alien4CloudToscaPlatform;
 
 public class EnablementIntegrationTest extends Alien4CloudIntegrationTest {
 
-    @Test(expectedExceptions = {IllegalStateException.class})
+    @Test(expectedExceptions = {PlanNotRecognizedException.class})
     public void testTransformerRejectsBlueprintWhenFeatureDisabled() throws Exception {
         BrooklynFeatureEnablement.disable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
         try {
@@ -47,7 +48,6 @@ public class EnablementIntegrationTest extends Alien4CloudIntegrationTest {
                 final Throwable firstInteresting = Exceptions.getFirstThrowableMatching(e, Predicates.instanceOf(IllegalStateException.class));
                 assertNotNull(firstInteresting, "Expected " + IllegalStateException.class.getName());
             }
-
         } finally {
             BrooklynFeatureEnablement.enable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
         }

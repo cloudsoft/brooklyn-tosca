d4a03c5590f34673b51c9dbd3a140ef97db3c487
Robert Moss <robert.moss@cloudsoftcorp.com>
2016-02-16 09:42:13 +0000

    change ToscaPlanToSpec to ToscaTypePlan

--- brooklyn-tosca-transformer/src/test/java/io/cloudsoft/tosca/a4c/brooklyn/EnablementIntegrationTest.java
+++ brooklyn-tosca-transformer/src/test/java/io/cloudsoft/tosca/a4c/brooklyn/EnablementIntegrationTest.java
@@ -5,11 +5,9 @@ import static org.testng.Assert.fail;
 
 import org.apache.brooklyn.api.entity.Application;
 import org.apache.brooklyn.api.entity.EntitySpec;
-import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatformLauncherNoServer;
 import org.apache.brooklyn.camp.brooklyn.spi.creation.CampTypePlanTransformer;
 import org.apache.brooklyn.core.BrooklynFeatureEnablement;
 import org.apache.brooklyn.core.plan.PlanNotRecognizedException;
-import org.apache.brooklyn.core.test.BrooklynAppUnitTestSupport;
 import org.apache.brooklyn.core.typereg.RegisteredTypeLoadingContexts;
 import org.apache.brooklyn.util.core.ResourceUtils;
 import org.apache.brooklyn.util.exceptions.Exceptions;
@@ -18,25 +16,23 @@ import org.testng.annotations.Test;
 import com.google.common.base.Predicates;
 
 import io.cloudsoft.tosca.a4c.Alien4CloudIntegrationTest;
-import io.cloudsoft.tosca.a4c.platform.Alien4CloudToscaPlatform;
 
 public class EnablementIntegrationTest extends Alien4CloudIntegrationTest {
 
     @Test(expectedExceptions = {PlanNotRecognizedException.class})
     public void testTransformerRejectsBlueprintWhenFeatureDisabled() throws Exception {
-        BrooklynFeatureEnablement.disable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
+        BrooklynFeatureEnablement.disable(ToscaTypePlanTransformer.FEATURE_TOSCA_ENABLED);
         try {
             // Should throw.
-            transformer.createApplicationSpec(
-                    new ResourceUtils(mgmt).getResourceAsString("classpath://templates/helloworld-sql.tosca.yaml"));
+            create("classpath://templates/helloworld-sql.tosca.yaml");
         } finally {
-            BrooklynFeatureEnablement.enable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
+            BrooklynFeatureEnablement.enable(ToscaTypePlanTransformer.FEATURE_TOSCA_ENABLED);
         }
     }
 
     @Test
     public void testCreateSpecFromPlanFailsWhenFeatureDisabled() throws Exception {
-        BrooklynFeatureEnablement.disable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
+        BrooklynFeatureEnablement.disable(ToscaTypePlanTransformer.FEATURE_TOSCA_ENABLED);
         try {
             try {
                 mgmt.getTypeRegistry().createSpecFromPlan(CampTypePlanTransformer.FORMAT,
@@ -49,7 +45,7 @@ public class EnablementIntegrationTest extends Alien4CloudIntegrationTest {
                 assertNotNull(firstInteresting, "Expected " + IllegalStateException.class.getName());
             }
         } finally {
-            BrooklynFeatureEnablement.enable(ToscaPlanToSpecTransformer.FEATURE_TOSCA_ENABLED);
+            BrooklynFeatureEnablement.enable(ToscaTypePlanTransformer.FEATURE_TOSCA_ENABLED);
         }
     }
 }

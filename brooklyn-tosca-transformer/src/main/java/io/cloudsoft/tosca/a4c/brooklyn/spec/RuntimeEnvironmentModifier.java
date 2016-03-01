112028ebb4f72eaa8a01a3d837211785d4b44d86
Martin Harris <github@nakomis.com>
2016-03-01 12:56:22 +0000

    Adds instance-specific directory prefix to runtime files (in install.dir)

--- brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/spec/RuntimeEnvironmentModifier.java
+++ brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/spec/RuntimeEnvironmentModifier.java
@@ -16,6 +16,7 @@ import org.apache.brooklyn.camp.brooklyn.spi.dsl.BrooklynDslDeferredSupplier;
 import org.apache.brooklyn.camp.brooklyn.spi.dsl.methods.BrooklynDslCommon;
 import org.apache.brooklyn.entity.software.base.SoftwareProcess;
 import org.apache.brooklyn.util.collections.MutableMap;
+import org.apache.brooklyn.util.text.Identifiers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -45,6 +46,11 @@ public class RuntimeEnvironmentModifier extends AbstractSpecModifier {
         }
 
         final Map<String, String> filesToCopy = MutableMap.of();
+        // As PRE_INSTALL_FILES is explicitly Map<String, String>, we can't use a deferred "run.dir" in the
+        // destination path, so we must use the install.dir, which is shared by all instances of the same
+        // type (with the exception of VanillaSoftwareProcess). In order to prevent collision between
+        // e.g. multiple instances of MySql on the same server, we must use an instance-specific prefix
+        final String directoryPrefix = Identifiers.makeRandomJavaId(6);
 
         for (final String artifactId : artifacts) {
             Optional<Path> optionalResourcesRootPath = getToscaFacade().getArtifactPath(nodeId, toscaApplication, artifactId);
@@ -53,15 +59,15 @@ public class RuntimeEnvironmentModifier extends AbstractSpecModifier {
             }
 
             // We know that formatString will return a BrooklynDslDeferredSupplier as the 2nd argument is not yet resolved
-            BrooklynDslDeferredSupplier deferredRunDir = (BrooklynDslDeferredSupplier) BrooklynDslCommon.formatString("%s/%s", BrooklynDslCommon.attributeWhenReady("install.dir"), artifactId);
-            entitySpec.configure(SoftwareProcess.SHELL_ENVIRONMENT.subKey(artifactId), deferredRunDir);
+            BrooklynDslDeferredSupplier deferredInstallDir = (BrooklynDslDeferredSupplier) BrooklynDslCommon.formatString("%s/%s/%s", BrooklynDslCommon.attributeWhenReady("install.dir"), directoryPrefix, artifactId);
+            entitySpec.configure(SoftwareProcess.SHELL_ENVIRONMENT.subKey(artifactId), deferredInstallDir);
 
             // Copy all files in resource.
             try {
                 Files.walkFileTree(optionalResourcesRootPath.get(), new SimpleFileVisitor<Path>() {
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        filesToCopy.put(file.toAbsolutePath().toString(), artifactId + "/" + file.getFileName().toString());
+                        filesToCopy.put(file.toAbsolutePath().toString(), String.format("%s/%s/%s", directoryPrefix, artifactId, file.getFileName().toString()));
                         return FileVisitResult.CONTINUE;
                     }
                 });
@@ -71,7 +77,7 @@ public class RuntimeEnvironmentModifier extends AbstractSpecModifier {
         }
 
         // It's not possible to infer from the artifact at what point these files will be used, so
-        // they need to be copied at the earliers point possible
+        // they need to be copied at the earliest point possible
         entitySpec.configure(SoftwareProcess.PRE_INSTALL_FILES, filesToCopy);
     }
 }

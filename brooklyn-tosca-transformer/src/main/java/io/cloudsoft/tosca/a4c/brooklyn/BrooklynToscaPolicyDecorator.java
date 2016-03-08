f4a61cf09dd23c56d1252875baf2609ab59b7b4e
Kiuby88 <kiuby88.programming@gmail.com>
2016-03-08 11:01:10 +0100

    Moving DSL management to AbstractToscaPolicyDecorator

--- brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/BrooklynToscaPolicyDecorator.java
+++ brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/BrooklynToscaPolicyDecorator.java
@@ -3,7 +3,6 @@ package io.cloudsoft.tosca.a4c.brooklyn;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.reflect.TypeToken;
 import io.cloudsoft.tosca.a4c.brooklyn.util.EntitySpecs;
 import org.apache.brooklyn.api.entity.Application;
 import org.apache.brooklyn.api.entity.EntitySpec;
@@ -12,11 +11,8 @@ import org.apache.brooklyn.api.mgmt.classloading.BrooklynClassLoadingContext;
 import org.apache.brooklyn.camp.brooklyn.BrooklynCampReservedKeys;
 import org.apache.brooklyn.camp.brooklyn.spi.creation.BrooklynEntityDecorationResolver;
 import org.apache.brooklyn.camp.brooklyn.spi.creation.BrooklynYamlTypeInstantiator;
-import org.apache.brooklyn.camp.brooklyn.spi.creation.CampCatalogUtils;
 import org.apache.brooklyn.core.mgmt.classloading.JavaBrooklynClassLoadingContext;
-import org.apache.brooklyn.util.collections.MutableMap;
 import org.apache.brooklyn.util.core.config.ConfigBag;
-import org.apache.brooklyn.util.core.flags.TypeCoercions;
 
 import java.util.List;
 import java.util.Map;
@@ -25,11 +21,10 @@ import java.util.Set;
 public class BrooklynToscaPolicyDecorator extends AbstractToscaPolicyDecorator {
 
     private EntitySpec<? extends Application> rootSpec;
-    private ManagementContext mgmt;
 
     BrooklynToscaPolicyDecorator(EntitySpec<? extends Application> rootSpec, ManagementContext mgmt) {
+        super(mgmt);
         this.rootSpec = rootSpec;
-        this.mgmt = mgmt;
     }
 
     public void decorate(Map<String, ?> policyData, String policyName, Optional<String> type, Set<String> groupMembers) {
@@ -41,11 +36,9 @@ public class BrooklynToscaPolicyDecorator extends AbstractToscaPolicyDecorator {
     }
 
     private ConfigBag getPolicyDefinition(String type, Map<String, ?> policyData) {
-
-        Map<String, ?> unresolvedProperties = getPolicyProperties(policyData);
         List<?> policies = ImmutableList.of(ImmutableMap.of(
                         "policyType", type,
-                        BrooklynCampReservedKeys.BROOKLYN_CONFIG, resolveProperties(unresolvedProperties)
+                        BrooklynCampReservedKeys.BROOKLYN_CONFIG, getPolicyProperties(policyData)
                 )
         );
         Map<?, ?> policyDefinition = ImmutableMap.of(BrooklynCampReservedKeys.BROOKLYN_POLICIES, policies);
@@ -73,33 +66,6 @@ public class BrooklynToscaPolicyDecorator extends AbstractToscaPolicyDecorator {
 
     private void decorateWithPolicy(BrooklynYamlTypeInstantiator.Factory yamlLoader, EntitySpec<?> spec, ConfigBag policyDefinition) {
         new BrooklynEntityDecorationResolver.PolicySpecResolver(yamlLoader).decorate(spec, policyDefinition);
-    }
-
-    private Map<String, Object> resolveProperties(Map<String, ?> policyProperties) {
-        Map<String, Object> resolvedPolicyProperties = MutableMap.of();
-        for (Map.Entry<String, ?> entry : policyProperties.entrySet()) {
-            Optional<Object> resolvedValue =
-                    resolveValue(entry.getValue(), Optional.<TypeToken>absent());
-            if (resolvedValue.isPresent()) {
-                resolvedPolicyProperties.put(entry.getKey(), resolvedValue.get());
-            } else {
-                resolvedPolicyProperties.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return resolvedPolicyProperties;
-    }
-
-    protected Optional<Object> resolveValue(Object unresolvedValue, Optional<TypeToken> desiredType) {
-        if (unresolvedValue == null) {
-            return Optional.absent();
-        }
-        // The 'dsl' key is arbitrary, but the interpreter requires a map
-        Map<String, Object> resolvedConfigMap = CampCatalogUtils.getCampPlatform(mgmt)
-                .pdp()
-                .applyInterpreters(ImmutableMap.of("dsl", unresolvedValue));
-        return Optional.of(desiredType.isPresent()
-                ? TypeCoercions.coerce(resolvedConfigMap.get("dsl"), desiredType.get())
-                : resolvedConfigMap.get("dsl"));
     }
 
 }

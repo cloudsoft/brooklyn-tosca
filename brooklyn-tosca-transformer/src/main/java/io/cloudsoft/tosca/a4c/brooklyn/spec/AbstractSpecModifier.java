441f556d16de724dcf7a5d0347aa1aed8ffd5f5f
Aled Sage <aled.sage@gmail.com>
2019-04-03 07:54:05 +0100

    Config resolving: handle DSL values
    The config value coercion broke when Brooklyn was changed to respect
    generics (e.g. a config value of type DSL cannot be coerced to a String,
    before it is evaluated - but this is when we’re creating the EntitySpec
    so we can’t evaluate it yet).
    
    Also more testing: a simple config key of type String could previously
    not be set with a DSL value.

--- brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/spec/AbstractSpecModifier.java
+++ brooklyn-tosca-transformer/src/main/java/io/cloudsoft/tosca/a4c/brooklyn/spec/AbstractSpecModifier.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.brooklyn.api.mgmt.ManagementContext;
 import org.apache.brooklyn.camp.brooklyn.BrooklynCampPlatform;
 import org.apache.brooklyn.util.core.flags.TypeCoercions;
+import org.apache.brooklyn.util.core.task.DeferredSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,9 +40,32 @@ public abstract class AbstractSpecModifier implements EntitySpecModifier {
         Map<String, Object> resolvedConfigMap = BrooklynCampPlatform.findPlatform(mgmt)
                 .pdp()
                 .applyInterpreters(ImmutableMap.of("dsl", unresolvedValue));
-        return Optional.of(desiredType.isPresent()
-                           ? TypeCoercions.coerce(resolvedConfigMap.get("dsl"), desiredType.get())
-                           : resolvedConfigMap.get("dsl"));
-    }
+        Object resolvedValue = resolvedConfigMap.get("dsl");
+        
+        if (resolvedValue instanceof DeferredSupplier) {
+            // Don't cast - let Brooklyn evaluate it later (the value is a resolved DSL expression).
+            return Optional.of(resolvedValue);
+        }
+        
+        if (desiredType.isPresent()) {
+            // Don't look at generics when casting.
+            // 
+            // Let Brooklyn do that later when it uses/evaluates the config. We just need to create 
+            // the EntitySpec object.
+            //
+            // This is important for DSL expressions (e.g. in a Map<String, String> such as 
+            // TomcatServer's javaSysProps, a DSL expression could not be coerced to a string.
+            //
+            // By stripping the generics, it restores the brooklyn behaviour prior to snapshot at 
+            // 31st Aug 2018 (commit 3e57b14b220bd7a994a9143d83bc123879086aff) when map/collection 
+            // generics were not respected during coercion.
+            
+            Class<?> desiredRawType = desiredType.get().getRawType();
 
+            return Optional.of(TypeCoercions.coerce(resolvedValue, desiredRawType));
+
+        } else {
+            return Optional.of(resolvedValue);
+        }
+    }
 }
